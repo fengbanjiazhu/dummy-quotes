650c2d7dbfd7d27a4f263e7be257094f7e25040f
fengbanjiazhu <562475805@qq.com>
2023-02-20 19:23:27 +1100

    add route to 404 page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="centered">
+      <p>404 Page Not Found</p>
+    </div>
+  );
+}
+
+export default NotFound;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -3,18 +3,33 @@ import { Route } from "react-router-dom";
 import { Fragment } from "react";
 import { useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Jeff",
+    text: "Learning react is fun!",
+  },
+  {
+    id: "q2",
+    author: "Jack",
+    text: "Learning react is great!",
+  },
+];
 
 function QuoteDetail() {
   const params = useParams();
 
+  let quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    quote = { text: "Quote Not Found", author: "404" };
+  }
   return (
     <Fragment>
-      <h1>Quote Detail page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author}></HighlightedQuote>
 
-      {/* <Route path="/quotes/:quoteId/comments"> */}
-
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path="/quotes/:quoteId/comments">
         <Comments></Comments>
       </Route>
     </Fragment>
